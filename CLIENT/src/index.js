f335d2590b211e59ad6644362cbd0fa053962351
AhmedMasterkkk <svdhswnfds@gmail.com>
2025-05-09 00:02:45 +0500

    added react-router-dom in pakage.json and set routes in index.js

--- CLIENT/src/index.js
+++ CLIENT/src/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+import AuthLayout from "../src/layouts/AuthLayout";
+import Login from "./pages/AUth/Login";
+import Register from "../src/pages/AUth/Register";
+import HomePage from "./pages/HomePage/Homepage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <HomePage/> },
+      {
+        path: "auth",
+        element: <AuthLayout />,
+        children:[
+        {
+          path: "Login",
+          element: <Login />,
+        },
+        {
+          path: "Register",
+          element: <Register/>,
+        }
+      ]
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
